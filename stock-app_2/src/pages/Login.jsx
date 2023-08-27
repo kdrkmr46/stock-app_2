f4b107910d15a40f3cbe5ddaf5677a1cca4ac603
Kadir Kumru <130503664+kdrkmr46@users.noreply.github.com>
2023-08-28 00:55:27 +0300

    login jsx finished

--- stock-app_2/src/pages/Login.jsx
+++ stock-app_2/src/pages/Login.jsx
@@ -27,7 +27,123 @@ const Login = () => {
         .matches(/\d+/, "En az bir rakam içermelidir")
         .matches(/[a-z]/, "En az bir küçük harf içermelidir")
         .matches(/[A-Z]/, "En az bir büyük harf içermelidir.")
-        .matches(/[!,?{}><%&$#])
+        .matches(/[!,?{}><%&$#£+-.]+/,"En az bir özel karakter içermelidir"),
 
     })
+return (
+    <Container maxWidth="lg">
+        <Grid
+        Container
+        justifyContent="center"
+        direction="row-reverse"
+        sx={{
+            height: "100vh",
+            p: 2,
+        }}
+        >
+            <Grid item xs={12}  mb={3}   >
+                <Typography variant="h3" color="primary" align="center"  >
+                    STOCK APP 
+
+                </Typography>
+
+            </Grid>
+            <Grid item xs={12} sm={10} md={6}  >
+                <Avatar
+                sx={{
+                    backgroundColor:"secondary.light",
+                    m: "auto",
+                    width: 40,
+                    height: 40,
+                }}
+                >
+                    <LockIcon size="30"  />
+
+                </Avatar>
+                <Typography
+                variant="h4"
+                align="center"
+                mb={4}
+                color="secondary.light"
+                >
+                    Login
+
+                </Typography>
+                <Formik
+                initialValues={{ email:"", password="" }}
+                validationSchema={loginSchema}
+                onSubmit={(values, action) => {
+                    login(values)
+                    action.resetForm()
+                    action.setSubmitting(false)
+                }
+                    }
+                >
+                    {({ handleChange, handleBlur, values, touched, errors})=> (
+                        <Form>
+<Box  sx={{   display: "flex", flexdirection: "column",  gap: 2
+
+}}   >
+    <TextField
+    label="Email"
+    name="email"
+    id="email"
+    type="email"
+    variant="outlined"
+    onChanged={handleChange}
+    onBlur={handleBlur}
+    value={values.email}
+    error={touched.email && Boolean(errors.email)}
+    helperText={touched.email}/>
+    
+    
+    <TextField
+    label="password"
+    name="password"
+    id="password"
+    type="password"
+    variant="outlined"
+    onChange={handleChange}
+    onBlur={handleBlur}
+    value={values.password}
+    error={touched.password && Boolean(errors.password) }
+    helperText={errors.password}/>
+    <Button variant="contained" type="submit">
+        Submit
+
+    </Button>
+    
+    
+
+</Box>
+
+
+                        </Form>
+                    )
+                    
+                    }
+
+                </Formik>
+                <Box sx={{textAlign: "center", mt: 2  }}
+                >
+                    <Link to="/register"  > Do you have not an account?
+                    </Link>
+
+                </Box>
+
+            </Grid>
+            <Grid item xs={10} sm={7} md={6}  >
+                <Container>
+                    <img src={image} alt="img"  />
+                </Container>
+
+            </Grid>
+
+        </Grid>
+
+    </Container>
+)
+
 }
+
+export default Login 
